f77eba06568a5b10a97699202ba6c98a3be7a144
akonisanchez <sanchezakoni@gmail.com>
2026-03-11 15:15:58 -0700

    Set up PacePilot React project with clean starter layout

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <main className="app">
+      <header className="hero">
+        <h1>PacePilot.io</h1>
+        <p>Your flexible weekly running plan generator.</p>
+      </header>
+    </main>
+  );
+}
+
+export default App;
